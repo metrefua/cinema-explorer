9e4d046505b47e3cace9bd8054e8a171ce32d04e
Metrefua <metrefuatessema@gmail.com>
2026-06-02 00:28:36 +0300

    UPDATE constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'https://api.themoviedb.org/3';
-export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
+export const API_BASE_URL = import.meta.env.VITE_TMDB_BASE_URL || 'https://api.themoviedb.org/3';
+export const IMAGE_BASE_URL = import.meta.env.VITE_TMDB_IMAGE_URL || 'https://image.tmdb.org/t/p';
 export const IMAGE_SIZES = {
   poster: 'w500',
   backdrop: 'original',
